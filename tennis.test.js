02d825bc76ee0ac80ec46270cc1f9662f790004e
Andrea Martin <amartin.onda@gmail.com>
2022-03-24 13:57:10 +0100

    Add failing test for the case of 'fifteen all'

--- tennis.test.js
+++ tennis.test.js
@@ -1,8 +1,13 @@
-const tennis = require ('./tennis');
+const Tennis = require ('./tennis');
 
 describe('tennis kata score', ()=>{
     test('returns love all', ()=>{
         const tennis = new Tennis();
         expect(tennis.score()).toBe('Love all');
-    })
+    });
+
+    test('returns fifteen all', ()=>{
+        const tennis = new Tennis();
+        expect(tennis.score()).toBe('fifteen all');
+    });
 })
