3f58a7507df089319e954fe9975d12f1b3e438d0
Andrea Martin <amartin.onda@gmail.com>
2022-03-24 14:16:14 +0100

    Pass test for the case of 'love fifteen'

--- tennis.js
+++ tennis.js
@@ -1,6 +1,7 @@
 class Tennis{
 
     playerOne = 0;
+    playerTwo = 0;
 
     scoreTable = {
         1: 'fifteen',
@@ -11,6 +12,9 @@ class Tennis{
     score(){
         if(this.playerOne >0){
             return `${this.scoreTable[this.playerOne]} love`;
+        }
+        if(this.playerTwo == 1){
+            return 'love fifteen';
         }
         return 'love all';
     }
