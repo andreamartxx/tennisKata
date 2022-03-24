0b165260dd326ec826cae466febd8428abf786f6
Andrea Martin <amartin.onda@gmail.com>
2022-03-24 14:06:59 +0100

    Pass test for the case of 'fifteen love'

--- tennis.js
+++ tennis.js
@@ -1,7 +1,14 @@
 class Tennis{
+
+    playerOne = 0;
+
     score(){
-        return 'Love all';
+        if(this.playerOne == 1){
+            return 'fifteen love';
+        }
+        return 'love all';
     }
+
 }
 
 module.exports = Tennis;
