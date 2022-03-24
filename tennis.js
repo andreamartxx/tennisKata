f0706ecad35bc6b1ba9d5efd8a6a9e0dc422939d
Andrea Martin <amartin.onda@gmail.com>
2022-03-24 14:13:36 +0100

    Refactor score cases playerOne

--- tennis.js
+++ tennis.js
@@ -2,15 +2,15 @@ class Tennis{
 
     playerOne = 0;
 
+    scoreTable = {
+        1: 'fifteen',
+        2: 'thirty',
+        3: 'forty'
+    }
+
     score(){
-        if(this.playerOne == 1){
-            return 'fifteen love';
-        }
-        if(this.playerOne == 2){
-            return 'thirty love';
-        }
-        if(this.playerOne == 3){
-            return 'forty love';
+        if(this.playerOne >0){
+            return `${this.scoreTable[this.playerOne]} love`;
         }
         return 'love all';
     }
